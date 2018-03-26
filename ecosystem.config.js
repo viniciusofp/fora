ba93b8d07faa8767d80db27dcce7cc1685a8afde
viniciusofp <viniciusofp@gmail.com>
2018-03-26 17:31:27 -0300

    some more questions

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ec2-user',
-      host: 'ec2-34-235-143-94.compute-1.amazonaws.com',
+      host: 'ec2-18-232-235-70.compute-1.amazonaws.com',
       key: '~/.ssh/fora.pem',
       ref: 'origin/master',
       repo: 'https://github.com/viniciusofp/fora.git',
