faf9bb9855af7d1d4365348f3fcf030aa2bc7569
viniciusofp <viniciusofp@gmail.com>
2018-05-10 11:26:39 -0300

    entrega versao testes

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+$(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+});
+
 $('#outro_genero_input').click(function() {
     $('.genero').prop('checked', false);
     $('#outro_genero').prop('checked', true);
@@ -18,34 +25,28 @@ $('#decisoes-outro-input').click(function() {
     $('#decisoes-outro').prop('checked', true);
 });
 $('#decisoes-outro').click(function() {
-    $('#decisoes-outro-input').addClass('animated bounceIn').focus();
+    $('#decisoes-outro-input').addClass('animated fadeIn').focus();
 });
 $('#outro-genero').click(function() {
-    $('#outro-genero-input').addClass('animated bounceIn').focus();
+    $('#outro-genero-input').addClass('animated fadeIn').focus();
 });
 $('#outra-cor').click(function() {
-    $('#outra-cor-input').addClass('animated bounceIn').focus();
+    $('#outra-cor-input').addClass('animated fadeIn').focus();
 });
 $('#outra-religiao').click(function() {
-    $('#outra-religiao-input').addClass('animated bounceIn').focus();
+    $('#outra-religiao-input').addClass('animated fadeIn').focus();
 });
 $('#acesso-outro').click(function() {
-    $('#acesso-outro-input').addClass('animated bounceIn').focus();
+    $('#acesso-outro-input').addClass('animated fadeIn').focus();
 });
 $('#oquevive-outro').click(function() {
-    $('#oquevive-outro-input').addClass('animated bounceIn').focus();
+    $('#oquevive-outro-input').addClass('animated fadeIn').focus();
 });
 $('#outra-frequencia').click(function() {
-    $('#outra-frequencia-input').addClass('animated bounceIn').focus();
-});
-$('#melhoraespacocasa-outro').click(function() {
-    $('#melhoraespacocasa-outro-input').addClass('animated bounceIn').focus();
-});
-$('#melhoraespacotrabalho-outro').click(function() {
-    $('#melhoraespacotrabalho-outro-input').addClass('animated bounceIn').focus();
+    $('#outra-frequencia-input').addClass('animated fadeIn').focus();
 });
 $('#meio-outro').click(function() {
-    $('#meio-outro-input').addClass('animated bounceIn').focus();
+    $('#meio-outro-input').addClass('animated fadeIn').focus();
 });
 
 
@@ -72,25 +73,24 @@ var questions = $('.question');
 
 $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
     var activeQ = $(e.relatedTarget)
-    if (activeQ.hasClass('gender')) {
-        $('.img-right-gender').fadeIn();
-    } else {
-        $('.img-right-gender').fadeOut();
-    }
-    if (activeQ.hasClass('oqvcfaz')) {
-        $('.img-right-oqvcfaz').fadeIn();
-    } else {
-        $('.img-right-oqvcfaz').fadeOut();
-    }
-    if (activeQ.hasClass('intro1')) {
-        $('.img-right-intro1').fadeIn();
-    } else {
-        $('.img-right-intro1').fadeOut();
-    }
-    if (activeQ.hasClass('freq')) {
-        $('.img-right-freq').fadeIn();
-    } else {
-        $('.img-right-freq').fadeOut();
+    var images = [
+        'gender',
+        'oqvcfaz',
+        'intro1',
+        'freq',
+        'qualoespaco',
+        'costumafazer',
+        'pqnsai',
+        'reflexao'
+    ]
+    for (var i = images.length - 1; i >= 0; i--) {
+        images[i]
+
+        if (activeQ.hasClass(images[i])) {
+            $('.img-right-' + images[i]).fadeIn();
+        } else {
+            $('.img-right-' + images[i]).fadeOut();
+        }
     }
 })
 
@@ -102,17 +102,21 @@ $('.carousel').each(function () {
         ]
     });
     hammertime.on('swipeleft', function () {
-        $carousel.carousel('next');
+        if ( window.innerWidth < 767) {
+            $carousel.carousel('next');
+        }
     });
     hammertime.on('swiperight', function () {
-        $carousel.carousel('prev');
+        if ( window.innerWidth < 767) {
+            $carousel.carousel('prev');
+        }
     });
 });
 
-$('.arlivre label, .frequencia label').click(function() {
+$('.arlivre label, .frequencia label, .goToNext').click(function() {
     setTimeout(function(){
         $('#carouselExampleControls').carousel('next')
-    }, 600)
+    }, 500)
 })
 $('#nenhuma').click(function() {
     $('#check-bar-progress').attr('class', "w0")
@@ -135,6 +139,8 @@ var quemmora = $('.quemmora');
 var quemtrabalhaestuda = $('.quemtrabalhaestuda');
 var quemmoratrabalhaestuda = $('.quemmoratrabalhaestuda');
 var quemnenhum = $('.quemnenhum');
+var pqnsai = $('.pqnsai');
+var costumafazer = $('.costumafazer');
 
 $('#sp-nenhum').change(function() {
     if($('#sp-nenhum')[0].checked == true) {
@@ -164,8 +170,11 @@ $('.quemmora').remove();
 $('.quemtrabalhaestuda').remove();
 $('.quemmoratrabalhaestuda').remove();
 $('.quemnenhum').remove();
+$('.pqnsai').remove();
+$('.costumafazer').remove();
 $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
     if (e.direction == 'left') {
+
         if ($(e.relatedTarget).hasClass('cep')) {
             $('.quemmora').remove();
             $('.quemtrabalhaestuda').remove();
@@ -180,12 +189,33 @@ $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
                 $('#estado-cep').val('SP');
             }
             if ($('#moro')[0].checked == true) {
-                quemmora.insertAfter('#cep-modal');
+                quemmora.insertAfter('#cep-modal');                $('#melhoraespacocasa-outro').click(function() {
+                    console.log('oi')
+                    $('#melhoraespacocasa-outro-input').addClass('animated fadeIn').focus();
+                });
             }
             if ($('#moro')[0].checked == false) {
                 if ($('#trabalho')[0].checked == true || $('#estudo')[0].checked == true) {
                     quemtrabalhaestuda.insertAfter('#cep-modal');
+                    $('#melhoraespacotrabalho-outro').click(function() {
+                        $('#melhoraespacotrabalho-outro-input').addClass('animated fadeIn').focus();
+                    });
                 }
+            }
+        }
+
+
+        if ($(e.relatedTarget).prev().hasClass('freq')) {
+
+            $('.pqnsai').remove();
+            $('.costumafazer').remove();
+            console.log('takeaction')
+
+            if ($('#quase-todo-dia')[0].checked == true || $('#finais-de-semana')[0].checked == true || $('#saio-pouco')[0].checked == true) {
+                costumafazer.insertAfter('.freq').addClass('active');
+            }
+            if ($('#saio-muito-pouco')[0].checked == true || $('#saio-muito-pouco-mesmo')[0].checked == true) {
+                pqnsai.insertAfter('.freq').addClass('active');
             }
         }
     }
@@ -195,7 +225,7 @@ $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
 
     var places = []
 
-    $('path').click(function() {
+    $('.map path').click(function() {
         $(this).toggleClass('selected');
         if ( places.indexOf($(this).attr('data-name')) > -1) {
             places = places.filter(e => e !== $(this).attr('data-name') );
@@ -205,14 +235,14 @@ $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
             $('#circulacao').val(places)
         }
     })
-    $('path').hover(function() {
+    $('.map path').hover(function() {
         $('#tail').text($(this).attr('data-name'))
         $('#tail').addClass('popover-visible')
     }).mouseleave(function() {
         $('#tail').text('')
         $('#tail').removeClass('popover-visible')
     })
-
+if ( $('.question-wrapper').length > 0 ) {
     $(document).bind('mousemove', function(e){
         $('#tail').css({
            left: e.pageX - $(document).scrollLeft() - $('.question-wrapper.active .question').offset().left,
@@ -222,6 +252,7 @@ $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
     $('#circulacao-submit').click(function() {
         $('#circulacao').val(places)
     })
+}
 
 
 
@@ -232,14 +263,21 @@ var questionsWidth =window.innerWidth / questionsLength;
 $('.question-bar').css('width', questionsWidth);
 
 $('#carouselExampleControls').on('slid.bs.carousel', function (e) {
-    if ($(e.relatedTarget).hasClass('secao-1')) {
-        $('.brand').css('fill', 'red')
+
+    setTimeout(function() {
+        $('.question-wrapper.active:not(.cep) .textinput').focus();
+
+        if (! $('.active').hasClass('not-required')) {
+            $('.next-icon').parent().removeAttr( "data-slide" )
+        }
+    }, 200)
+
+    $('.slider-nav').addClass('totheright');
+    if ($(e.relatedTarget).hasClass('abre')) {
+        $('.brand').css('fill', 'white')
     }
-    if ($(e.relatedTarget).hasClass('secao-2')) {
-        $('.brand').css('fill', 'orange')
-    }
-    if ($(e.relatedTarget).hasClass('secao-3')) {
-        $('.brand').css('fill', 'blue')
+    if ($(e.relatedTarget).hasClass('contracapa')) {
+        $('.brand').css('fill', '#ff00ff')
     }
     var questions = $('.question');
     var questionsLength = questions.length;
@@ -255,9 +293,7 @@ $('#carouselExampleControls').on('slid.bs.carousel', function (e) {
 
 
 
-$('.next-icon').click(function() {
-    // $('.carousel').carousel('prev');
-
+$('.next-icon').hover(function() {
     var thisQ = $('.active .question')
     var qChildren = thisQ.find('input, textarea, select')
     if ($('.active').hasClass('not-required')) {} else {
@@ -280,17 +316,57 @@ $('.next-icon').click(function() {
             })
             if (! hasAnswer) {
                 setTimeout(function() {
-                    $('.carousel').carousel('prev');
-                    $('#alert').modal('show')
+                    console.log($('.next-icon'))
+                    $('.next-icon').parent().removeAttr( "data-slide" )
+                    $('.alert').fadeIn()
+                    setTimeout(function() {
+                        $('.alert').fadeOut()
+                    }, 1000)
                 }, 300)
-
+            } else {
+                $('.next-icon').parent().attr( "data-slide", 'next' )
             }
         } else {
             console.log('não tem input')
+            $('.next-icon').parent().attr( "data-slide", 'next' )
         }
     }
-
+}, function() {
+    var thisQ = $('.active .question')
+    var qChildren = thisQ.find('input, textarea, select')
+    if ($('.active').hasClass('not-required')) {} else {
+        if (qChildren.length > 0) {
+            var hasAnswer = false ;
+            qChildren.each(function(i, el) {
+                if ($(el).attr('type') == 'text' || $(el).attr('type') == 'number') {
+                    if ($(el).val() != "") {
+                        hasAnswer = true;
+                    }
+                } else if ($(el).attr('type') == 'checkbox' || $(el).attr('type') == 'radio') {
+                    if (el.checked == true) {
+                        hasAnswer = true;
+                    }
+                } else if (el.tagName == 'SELECT') {
+                    if ($(el).val() != "") {
+                        hasAnswer = true;
+                    }
+                }
+            })
+            if (! hasAnswer) {
+                setTimeout(function() {
+                    console.log($('.next-icon'))
+                    $('.next-icon').parent().removeAttr( "data-slide" )
+                }, 300)
+            } else {
+                $('.next-icon').parent().attr( "data-slide", 'next' )
+            }
+        } else {
+            console.log('não tem input')
+            $('.next-icon').parent().attr( "data-slide", 'next' )
+        }
+    }
 })
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ var url = 'mongodb://localhost:27017/fora';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  // Set user ID if it doesn't exist
   if (req.cookies.userid) {
     console.log('I have an ID already, thank you')
   } else {
@@ -15,24 +17,35 @@ router.get('/', function(req, res, next) {
     res.cookie('userid', userid)
   }
 
+  // Connect to database and insert (or update) form
   MongoClient.connect(url, function(err, database) {
     if (err) {
       throw err;
     }
     var data = [];
     const db = database.db('fora').collection('q');
+    req.body.timestampBegin = new Date( Date.now() );
+    req.body.userid = userid;
+    var ip = req.connection.remoteAddress ||
+             req.socket.remoteAddress ||
+             req.connection.socket.remoteAddress;
+    console.log(ip)
 
-
-    db.find().toArray(function(err, result) {
-      if (err) {
-        throw err;
+    db.find({userid: userid}).toArray(function(err, result) {
+      if (result.length > 0) {
+        db.findOneAndUpdate({userid: userid}, req.body)
+        console.log('já preencheu, update')
+      } else {
+        db.insert(req.body)
       }
-      res.render('parte1', { title: 'Fora', data: result });
-    });
+    })
+
+      res.render('parte1', { title: 'Fora - Questionário' });
   });
 });
-/* GET home page. */
-router.get('/parte-dois', function(req, res, next) {
+
+/* TELA FINAL */
+router.get('/fim', function(req, res, next) {
   MongoClient.connect(url, function(err, database) {
     if (err) {
       throw err;
@@ -50,16 +63,19 @@ router.get('/parte-dois', function(req, res, next) {
   });
 });
 
+// POST FORM
 router.post('/submit', function(req, res) {
     MongoClient.connect(url, function (err,database) {
         if (err) {
             console.log('Could not connect to DB')
         } else {
             const db = database.db('fora').collection('q');
-            req.body.timestamp = new Date( Date.now() );
+            req.body.timestampEnd = new Date( Date.now() );
             req.body.userid = req.cookies.userid;
             db.find({userid: req.cookies.userid}).toArray(function(err, result) {
               if (result.length > 0) {
+                console.log(result);
+                req.body.timestampBegin = result[0].timestampBegin;
                 db.findOneAndUpdate({userid: req.cookies.userid}, req.body)
                 console.log('já preencheu, update')
               } else {
@@ -67,8 +83,27 @@ router.post('/submit', function(req, res) {
               }
             })
         }
+      res.redirect('/fim')
+    })
+
+
+})
+router.post('/emailform', function(req, res) {
+    MongoClient.connect(url, function (err,database) {
+        if (err) {
+            console.log('Could not connect to DB')
+        } else {
+            const db = database.db('fora').collection('q');
+            db.find({userid: req.cookies.userid}).toArray(function(err, result) {
+              if (result.length > 0) {
+                console.log(result);
+                result[0].email = req.body.email;                db.findOneAndUpdate({userid: req.cookies.userid}, result[0])
+                console.log('já preencheu, update')
+              }
+            })
+        }
       res.clearCookie("userid");
-      res.redirect('/parte-dois')
+      res.redirect('/')
     })
 
 
